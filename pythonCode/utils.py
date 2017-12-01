25056e655f5c2626e73d97be8b5d4d6f0ee05cdc
Antoine Rondelet <rondelet.antoine@gmail.com>
2017-12-01 20:53:41 +0900

    Cannot parallelize traceGeneration because of traffic interference on same socket. Did it sequentially

---
+++ pythonCode/utils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class TrafficWeight:
+  def __init__(self):
+    self.request = 0
+    self.response = 0
+
+class PacketInterval:
+  def __init__(self):
+    self.firstPacket = 0
+    self.lastPacket = 0
